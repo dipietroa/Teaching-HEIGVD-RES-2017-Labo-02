30503fc19c308c0153ef03c8b77fc92d8a395859
Gallouche <gallandat.t@gmail.com>
2017-03-27 20:48:01 +0200

    impl of the test from testV1

--- QuizRouletteServer-build/QuizRouletteServer-test/src/test/java/ch/heigvd/res/labs/roulette/net/client/RouletteV2DipietroaTest.java
+++ QuizRouletteServer-build/QuizRouletteServer-test/src/test/java/ch/heigvd/res/labs/roulette/net/client/RouletteV2DipietroaTest.java
@@ -26,9 +26,58 @@ public class RouletteV2DipietroaTest {
 
     @Test
     @TestAuthor(githubId = {"Gallouche","dipietroa"})
-    public void serverShouldRun(){
-        assertTrue(roulettePair.server.isRunning());
+    public void theTestRouletteServerShouldRunDuringTests() throws IOException {
+        assertTrue(roulettePair.getServer().isRunning());
     }
 
+    @Test
+    @TestAuthor(githubId = {"Gallouche","dipietroa"})
+    public void theTestRouletteClientShouldBeConnectedWhenATestStarts() throws IOException {
+        assertTrue(roulettePair.getClient().isConnected());
+    }
 
+    @Test
+    @TestAuthor(githubId = {"Gallouche","dipietroa"})
+    public void itShouldBePossibleForARouletteClientToConnectToARouletteServerV2() throws Exception {
+        int port = roulettePair.getServer().getPort();
+        IRouletteV2Client client = new RouletteV2ClientImpl();
+        assertFalse(client.isConnected());
+        client.connect("localhost", port);
+        assertTrue(client.isConnected());
+    }
+
+    @Test
+    @TestAuthor(githubId = {"Gallouche","dipietroa"})
+    public void theServerShouldReturnTheCorrectVersionNumber() throws IOException {
+        assertEquals(RouletteV2Protocol.VERSION, roulettePair.getClient().getProtocolVersion());
+    }
+
+    @Test
+    @TestAuthor(githubId = {"Gallouche","dipietroa"})
+    public void theServerShouldHaveZeroStudentsAtStart() throws IOException {
+        int port = roulettePair.getServer().getPort();
+        IRouletteV2Client client = new RouletteV2ClientImpl();
+        client.connect("localhost", port);
+        int numberOfStudents = client.getNumberOfStudents();
+        assertEquals(0, numberOfStudents);
+    }
+
+    @Test
+    @TestAuthor(githubId = {"Gallouche","dipietroa"})
+    public void theServerShouldStillHaveZeroStudentsAtStart() throws IOException {
+        assertEquals(0, roulettePair.getClient().getNumberOfStudents());
+    }
+
+    @Test
+    @TestAuthor(githubId = {"Gallouche","dipietroa"})
+    public void theServerShouldCountStudents() throws IOException {
+        IRouletteV2Client client = roulettePair.getClient();
+        assertEquals(0, client.getNumberOfStudents());
+        client.loadStudent("sacha");
+        assertEquals(1, client.getNumberOfStudents());
+        client.loadStudent("olivier");
+        assertEquals(2, client.getNumberOfStudents());
+        client.loadStudent("fabienne");
+        assertEquals(3, client.getNumberOfStudents());
+    }
 }
